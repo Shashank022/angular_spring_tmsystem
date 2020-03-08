591cb85870610f355d91027fff52e8470a3f9292
Shashank <shashank022@gmail.com>
2020-03-07 21:31:19 -0500

    New changes

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -11,7 +11,7 @@ export class EventsComponent {
 
   private APP_URL : string = "http://localhost:5020";
   public response: any;
-  public isVisible : boolean;
+  public isVisible : boolean = false;
 
   constructor(private http:HttpClient) {
   }
@@ -26,6 +26,37 @@ export class EventsComponent {
         console.log('Error occured', error);
       }
     );
+  }
+
+  getEventDetails(eventid){
+    this.http.get(this.APP_URL + '/eventlist/').subscribe(
+      data => {
+        this.response = data;
+        this.isVisible = true;
+        console.log(data);
+      },
+      error => {
+        console.log('Error occured', error);
+      }
+    );
+
+  }
+
+  deleteEvent(eventid){
+    this.http.delete(this.APP_URL + '/eventlist/'+ eventid).subscribe(
+      data => {
+        this.response = data;
+        console.log(data);
+      },
+      error => {
+        console.log('Error occured', error);
+      }
+    );
+    this.refreshPage();
+  }
+
+  refreshPage() {
+   document.defaultView.location.reload();
   }
 
 
